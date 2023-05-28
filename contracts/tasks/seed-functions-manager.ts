24c081d5eb071f2a4f2a3cbc86cda3f9688a746f
amith4m <amith4m@gmail.com>
2023-05-27 21:04:17 -0500

    Fixed seed functions task

--- contracts/tasks/seed-functions-manager.ts
+++ contracts/tasks/seed-functions-manager.ts
@@ -1,10 +1,34 @@
 import { task, types } from "hardhat/config";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import fs from "fs";
 import { FunctionsManager } from "../typechain-types";
 import { networks } from "../hardhat.config";
+import { Signer } from "ethers";
 
-task("seed-functions-manager", "runs a function")
+type DemoConfig = {
+  functionId: string;
+  owner: Signer;
+  register: FunctionsManager.FunctionsRegisterRequestStruct;
+  execute: {
+    callers: Record<string, { caller: Signer; args: string[] }>;
+    gasLimit: number;
+  };
+};
+
+const imageUrls = {
+  maxHeadroom: "https://i.imgur.com/VE7uGB9.gif",
+  //TODO Swap below with the official version (Google Drive link: https://drive.google.com/drive/folders/1owvdEhRDtmwxE-7cAdxBXhjswiDPDT9W) uploaded on the vercel app since we can't get a direct link from google drive
+  coingecko:
+    "https://i.pinimg.com/736x/be/c9/b3/bec9b33d6638ff927a96d0e93546a056.jpg",
+  calculateApy:
+    "https://upload.wikimedia.org/wikipedia/commons/1/18/Simple_calculator.svg",
+  ethLogo:
+    "https://upload.wikimedia.org/wikipedia/commons/6/6f/Ethereum-icon-purple.svg",
+};
+
+task(
+  "seed-functions-manager",
+  "registers multiple functions under different owners and executes each with different users"
+)
   .addOptionalParam(
     "functionsmanager",
     "The address of the function manager",
@@ -17,66 +41,83 @@ task("seed-functions-manager", "runs a function")
     true,
     types.boolean
   )
-  .addOptionalParam(
-    "numrunsfloor",
-    "The minimum number of times to run each demo",
-    1,
-    types.int
-  )
-  .addOptionalParam(
-    "numruns",
-    "The number of times to each demo the function",
-    5,
-    types.int
-  )
   .setAction(async (taskArgs, { ethers, network }) => {
     console.log("Building demos");
 
-    const imageUrls = {
-      maxHeadroom: "https://i.imgur.com/VE7uGB9.gif",
-      //TODO Swap below with the official version (Google Drive link: https://drive.google.com/drive/folders/1owvdEhRDtmwxE-7cAdxBXhjswiDPDT9W) uploaded on the vercel app since we can't get a direct link from google drive
-      coingecko:
-        "https://i.pinimg.com/736x/be/c9/b3/bec9b33d6638ff927a96d0e93546a056.jpg",
-      calculateApy:
-        "https://upload.wikimedia.org/wikipedia/commons/1/18/Simple_calculator.svg",
-      ethLogo:
-        "https://upload.wikimedia.org/wikipedia/commons/6/6f/Ethereum-icon-purple.svg",
-    };
+    if (!process.env.FUNCTIONS_SUBSCRIPTION_ID) {
+      throw Error("FUNCTIONS_SUBSCRIPTION_ID must be set");
+    }
 
-    const extractSourceFromDemo = (demoPath: string) => {
-      const content = fs.readFileSync(demoPath, "utf-8").toString();
-      let start = -1;
-      let end = -1;
-      content.split("\n").forEach((line, i) => {
-        if (line.match(".*BEGIN[ _]SRC.*")) {
-          start = i;
-        } else if (line.match(".*END[ _]SRC.*")) {
-          end = i;
-        }
-      });
-      if (start === -1 || end === -1) {
-        throw new Error("Could not find start or end of snippet");
-      }
-      return content.split("\n").slice(start, end).join("\n");
-    };
+    if (!taskArgs.functionsmanager) {
+      throw new Error("--functionsmanager must be specified");
+    }
 
-    type DemoConfig = {
-      file: string;
-      functionId: string;
-      register: FunctionsManager.FunctionsRegisterRequestStruct;
-      execute: {
-        validArgs: string[][];
-        gasLimit: number;
-      };
-    };
+    const [functionsManagerOwner, user1, user2, user3, user4] =
+      await ethers.getSigners();
+    const signers = [functionsManagerOwner, user1, user2, user3, user4];
+
     const demos: DemoConfig[] = [
       {
-        file: "./demos/coingecko-price.ts",
         functionId:
-          "0x9293c329e8683a7d598bb8959885b177a198fdf79ae212997c1a2b2782b290dc",
+          "0x5f5cad2778707654691b9276b6efcc451154287a5e6f1d24a3a068799348619c",
+        owner: user1,
         register: {
           fees: ethers.utils.parseEther("0.02"),
-          functionName: "CoinGecko Price2",
+          functionName: "Compound Interest",
+          desc: "Calculate a continuously compounding interested rate",
+          imageUrl: imageUrls.calculateApy,
+          expectedArgs: ["Principal Amount", "APY times 100"],
+          codeLocation: 0,
+          secretsLocation: 0,
+          language: 0,
+          category: ethers.utils.formatBytes32String("Calculations"),
+          subId: process.env.FUNCTIONS_SUBSCRIPTION_ID,
+          source: fs.readFileSync("./demos/compound-interest.js").toString(),
+          secrets: [],
+        },
+        execute: {
+          callers: {
+            [user2.address]: { caller: user2, args: ["1000000", "450"] },
+            [user3.address]: { caller: user3, args: ["1000000", "500"] },
+            [user4.address]: { caller: user4, args: ["2500000", "300"] },
+          },
+          gasLimit: 500_000,
+        },
+      },
+      {
+        functionId:
+          "0xa3e7e69dd8bac0c4506af68d25c5aff3e163493cce345f187e34906ccc936bb8",
+        owner: user2,
+        register: {
+          fees: ethers.utils.parseEther("0.02"),
+          functionName: "Geometric Mean Example",
+          desc: "Calculate a geometric mean",
+          imageUrl: imageUrls.calculateApy,
+          expectedArgs: ["Varying numeric args"],
+          codeLocation: 0,
+          secretsLocation: 0,
+          language: 0,
+          category: ethers.utils.formatBytes32String("Calculations"),
+          subId: process.env.FUNCTIONS_SUBSCRIPTION_ID,
+          source: fs.readFileSync("./demos/geometric-mean.js").toString(),
+          secrets: [],
+        },
+        execute: {
+          callers: {
+            [user1.address]: { caller: user1, args: ["1", "2"] },
+            [user3.address]: { caller: user3, args: ["100", "200", "300"] },
+            [user4.address]: { caller: user4, args: ["12", "9", "23", "6"] },
+          },
+          gasLimit: 500_000,
+        },
+      },
+      {
+        functionId:
+          "0x144853f43d3b8e966203b3f8ae15dae36c91b9269c55fb4114d92e24c82b6df4",
+        owner: user3,
+        register: {
+          fees: ethers.utils.parseEther("0.05"),
+          functionName: "CoinGecko Price",
           desc: "Fetches a given price pair from CoinGecko",
           imageUrl: imageUrls.coingecko,
           expectedArgs: [
@@ -87,89 +128,63 @@ task("seed-functions-manager", "runs a function")
           secretsLocation: 0,
           language: 0,
           category: ethers.utils.formatBytes32String("Price Feed"),
-          subId: 941, //TODO fix this, it'll break when you run in prod
-          source: extractSourceFromDemo("./demos/coingecko-price.ts"),
+          subId: process.env.FUNCTIONS_SUBSCRIPTION_ID,
+          source: fs.readFileSync("./demos/coingecko-price.js").toString(),
           secrets: [],
         },
         execute: {
-          validArgs: [
-            ["ethereum", "usd"],
-            ["bitcoin", "usd"],
-            ["ripple", "jpy"],
-            ["ethereum", "eur"],
-          ],
+          callers: {
+            [user1.address]: { caller: user1, args: ["ethereum", "usd"] },
+            [user2.address]: { caller: user2, args: ["bitcoin", "usd"] },
+            [user4.address]: { caller: user4, args: ["ethereum", "eur"] },
+          },
+          gasLimit: 500_000,
+        },
+      },
+      {
+        functionId:
+          "0xb7ced75f5bc1d50bd5cfebba6436321806d7fdb1e31c1e9ebf3440fd6fe66707",
+        owner: user4,
+        register: {
+          fees: ethers.utils.parseEther("0.05"),
+          functionName: "CryptoCompare Price",
+          desc: "Fetches a given price pair from CryptoCompare",
+          imageUrl: imageUrls.ethLogo,
+          expectedArgs: ["Base", "Quote"],
+          codeLocation: 0,
+          secretsLocation: 0,
+          language: 0,
+          category: ethers.utils.formatBytes32String("Price Feed"),
+          subId: process.env.FUNCTIONS_SUBSCRIPTION_ID,
+          source: fs.readFileSync("./demos/cryptocompare-price.js").toString(),
+          secrets: [],
+        },
+        execute: {
+          callers: {
+            [user1.address]: { caller: user1, args: ["ETH", "USD"] },
+            [user2.address]: { caller: user2, args: ["BTC", "USD"] },
+            [user3.address]: { caller: user3, args: ["BTC", "ETH"] },
+          },
           gasLimit: 500_000,
         },
       },
     ];
     console.log("Finished bootstrapping demos");
 
-    if (!taskArgs.functionsmanager) {
-      throw new Error("--functionsmanager must be specified");
-    }
-
-    const [functionsManagerOwner, user1, user2, user3] =
-      await ethers.getSigners();
-
     const functionsManagerRaw = await ethers.getContractAt(
       "FunctionsManager",
       taskArgs.functionsmanager
     );
 
-    // Read users into an object of [address]: SignerWithAddress
-    const demoUsers = [functionsManagerOwner, user1, user2, user3].reduce(
-      (
-        acc: {
-          [key: string]: SignerWithAddress;
-        },
-        u
-      ) => {
-        acc[u.address] = u;
-        return acc;
-      },
-      {}
-    );
-
     console.log("Function manager owner: ", functionsManagerOwner.address);
-    console.log("Demo users: ", Object.keys(demoUsers));
-    const DEMO_OWNERS = process.env.DEMO_OWNERS?.split(",");
-    console.log("Demo owners: ", DEMO_OWNERS);
-    if (!DEMO_OWNERS || DEMO_OWNERS.length < demos.length) {
-      console.log("Demo owners not specified");
-      throw new Error(
-        "Please specify DEMO_OWNERS as a comma separated string of addresses. " +
-          "This collection must have an equal length to the number of demos in demo-config.ts" +
-          "Note, you can use the same account for multiple demos"
-      );
-    }
-    if (DEMO_OWNERS?.length < demos.length) {
-      DEMO_OWNERS.forEach((owner) => {
-        if (!demoUsers[owner]) {
-          console.log("Demo user not in PRIVATE_KEYS");
-          throw new Error(`Demo user ${owner} not in PRIVATE_KEYS`);
-        }
-      });
-    }
 
     if (taskArgs.registerfunctions) {
       console.log("Registering functions...");
       for (let i = 0; i < demos.length; i++) {
         const demo = demos[i];
-        const owner = demoUsers[DEMO_OWNERS[i]];
+        const owner = demo.owner;
         const localFm = functionsManagerRaw.connect(owner);
 
-        console.log("Checking if function with id", demo.functionId, "exists");
-        const functionExists = await localFm.getFunction(demo.functionId);
-        if (
-          functionExists &&
-          functionExists.owner !== ethers.constants.AddressZero
-        ) {
-          console.log("Function already exists, skipping registration");
-          continue;
-        }
-        console.log("Got function", functionExists);
-
-        console.log("Got function.owner", functionExists.owner);
         const registerCall = await localFm.registerFunction(demo.register, {
           gasLimit: 2_000_000,
         });
@@ -179,70 +194,81 @@ task("seed-functions-manager", "runs a function")
           demo.register.functionName
         );
 
-        // TODO check for FunctionRegistered event
         const registerEvent = receipt.events?.find(
           (e) => e.event === "FunctionRegistered"
         );
         if (!registerEvent) {
           throw new Error("No FunctionRegistered event found");
         }
-        console.log("FunctionRegistered event: ", registerEvent.args);
+        if (registerEvent.args) {
+          console.log(
+            `Generated Function ID ${registerEvent.args[0]} for ${demo.register.functionName}`
+          );
+          demo.functionId = registerEvent.args[0];
+        }
       }
     }
-    console.log(
-      "All known function ids: ",
-      await functionsManagerRaw.getFunctionIds()
-    );
 
     const linkTokenRaw = await ethers.getContractAt(
       "LinkToken",
       networks[network.name].linkToken
     );
 
+    for (let i = 0; i < signers.length; i++) {
+      const signer = signers[i];
+      console.log(`${signer.address} approving functions manager as spender `);
+
+      const linkToken = linkTokenRaw.connect(signer);
+      const approveTx = await linkToken.approve(
+        functionsManagerRaw.address,
+        ethers.utils.parseEther("10")
+      );
+      // Wait for LINK approval
+      await approveTx.wait(1);
+    }
+
     // Execute each function
     for (let i = 0; i < demos.length; i++) {
       const demo = demos[i];
-      const demoOwner = demoUsers[DEMO_OWNERS[i]];
       const functionId = demo.functionId;
-      const runs =
-        Math.floor(Math.random() * taskArgs.numruns) + taskArgs.numrunsfloor;
-      for (let j = 0; j < runs; j++) {
-        console.log(`Starting run ${j}/${runs} for ${functionId}`);
-        // const callerIdx = Math.floor(Math.random() * 1);
-        const callerIdx = Math.floor(Math.random() * 3);
-        const caller = [user1, user2, user3][callerIdx];
-
-        const functionManagerWithCaller = await functionsManagerRaw.connect(
-          caller
+      const callerList = Object.entries(demo.execute.callers);
+      for (let j = 0; j < callerList.length; j++) {
+        console.log(
+          `Starting run ${j + 1}/${callerList.length} for ${functionId}`
         );
-        const functionManagerWithOwner = await functionsManagerRaw.connect(
-          functionsManagerOwner
-        );
-        console.log("Approving token spender for caller", caller.address);
+        const [callerAddr, requestInfo] = callerList[j];
 
-        const linkToken = linkTokenRaw.connect(caller);
-        const approveTx = await linkToken.approve(
-          functionsManagerRaw.address,
-          ethers.utils.parseEther("10"),
-          { gasLimit: 1_000_000 }
+        const functionManagerWithCaller = functionsManagerRaw.connect(
+          functionsManagerOwner // TODO: change to unique callers
         );
 
-        const approveReceipt = await approveTx.wait(1);
-        const argsIdx = Math.floor(
-          Math.random() * demo.execute.validArgs.length
+        console.log(
+          `Executing function ${demo.register.functionName} as ${functionsManagerOwner.address}`
         );
-        const args = demo.execute.validArgs[argsIdx];
         const tx = await functionManagerWithCaller.executeRequest(
           functionId,
-          args,
-          900_000,
+          requestInfo.args,
+          300_000,
           {
-            gasLimit: 1_000_000,
+            gasLimit: 2_000_000,
           }
         );
         const execReceipt = await tx.wait(1);
-        console.log(`Finished run ${j}/${runs} for ${functionId}`);
-        console.log(execReceipt.events);
+        console.log(
+          `Finished run ${j + 1}/${callerList.length} for ${
+            demo.register.functionName
+          }`
+        );
+        const calledEvent = execReceipt.events?.find(
+          (e) => e.event === "FunctionCalled"
+        );
+        if (!calledEvent) {
+          throw new Error("No FunctionCalled event found");
+        }
+        if (calledEvent.args) {
+          console.log(`Called ${demo.register.functionName}`);
+          console.log(`Request ID: ${calledEvent.args[1]}`);
+        }
       }
     }
 
